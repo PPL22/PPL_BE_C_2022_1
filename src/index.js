a7eb3b746e804892dcdf41497dca0271c1a57754
liemroym <liemroymarcelino@gmail.com>
2022-09-20 12:34:50 +0700

    Added database for provinsi & kabupaten/kota

--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.use(express.json())
 
 
 require('dotenv').config()
 
-import { PrismaClient } from '@prisma/client'
+const { PrismaClient } = require('@prisma/client')
 const bcrypt = require('bcrypt');
 const prisma = new PrismaClient()
 const jwt = require('jsonwebtoken')
 
 app.post('/login', async (req, res) => {
-    const username = req.body.username
-    const password = req.body.password
+    const {username, password}= req.body
+    
+    console.log(req.body)
 
-    const user = await prisma.user.findUnique({
+    const user = await prisma.tb_user.findMany({
         where: {
             username: username
         }
     })
 
-    if (await bcrypt.compare(password, user.password)) {
-        const userAccessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-        const userRefreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-        res.json({
-            username: user.username,
-            role: user.role,
+    if (!user) return res.status(403).json({message: "User not found"})
+
+    if (password === user[0].password) {
+        const userAccessToken = jwt.sign({username: user[0].username}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15s'})
+        const userRefreshToken = jwt.sign({username: user[0].username}, process.env.REFRESH_TOKEN_SECRET)
+        return res.json({
+            username: user[0].username,
+            role: user[0].role,
             accessToken: userAccessToken,
             refreshToken: userRefreshToken
         })
+    } else {
+        return res.status(403).json({message: "Wrong password"})
     }
 })
 
