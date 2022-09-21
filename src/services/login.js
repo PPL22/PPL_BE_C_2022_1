568590d49493d10f5ecf3818a03f8d58644d6c2c
liemroym <liemroymarcelino@gmail.com>
2022-09-21 12:25:24 +0700

    Preparing for deployment

--- src/services/login.js
+++ src/services/login.js
@@ -17,9 +17,26 @@ const login = async (username, password) => {
         // Create new token
         const userAccessToken = jwt.sign({username: user.username, role: user.role}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15s'})
         const userRefreshToken = jwt.sign({username: user.username, role: user.role}, process.env.REFRESH_TOKEN_SECRET)
+        
+        // Cari owner
+        let owner, firstTime = false
+        if (user.role === "Mahasiswa") {
+            owner = await prisma.tb_mhs.findUnique({
+                where: user.username
+            })
+            if (!owner.email) {
+                firstTime = true
+            }
+        } else {
+            owner = await prisma.tb_dosen.findUnique({
+                where: user.username
+            })
+        }
+
         return {
             username: user.username,
             role: user.role,
+            firstTime: firstTime,
             accessToken: userAccessToken,
             refreshToken: userRefreshToken
         }
