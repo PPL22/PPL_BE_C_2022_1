b47038a37ee6a48e1df72249968184021eccae89
liemroym <liemroymarcelino@gmail.com>
2022-09-21 12:26:43 +0700

    Preparing for deployment

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 const router = require('./routes/index')
 
 app.use(express.json())
+app.use(cors())
 app.use('/', router)
 
 app.listen(port, () => {
